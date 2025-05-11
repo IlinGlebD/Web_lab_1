00a5ab2374ef272facf9764a635ef6fdbd5d66ef
Ильин Глеб <ilingleb1608@gmail.com>
2025-05-11 17:00:01 +0700

    Книги  (редактирование)

--- lab6/books.js
+++ lab6/books.js
@@ -2,16 +2,81 @@ const books = [
     {
         author: 'Лао-цзы',
         name: 'Дао дэ цзин. Канон о Дао и дэ',
-        publicationYear: 2022
+        publicationYear: 2022,
+        price: 520,
+        bestseller: true,
+        available: true
     },
     {
         author: 'Карл Саган',
         name: 'Космос',
-        publicationYear: 2024
+        publicationYear: 2024,
+        price: 690,
+        bestseller: true,
+        available: false
     },
     {
         author: 'Баладжи Касал',
         name: 'Ошибки разумного инвестора',
-        publicationYear: 2025
+        publicationYear: 2025,
+        price: 490,
+        bestseller: false,
+        available: true
     },
-]
+    {
+        author: 'Стивен Хокинг',
+        name: 'Краткая история времени',
+        publicationYear: 2021,
+        price: 450,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Нассим Талеб',
+        name: 'Черный лебедь',
+        publicationYear: 2019,
+        price: 610,
+        bestseller: false,
+        available: false
+    },
+    {
+        author: 'Юваль Харари',
+        name: 'Sapiens',
+        publicationYear: 2020,
+        price: 700,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Ричард Докинз',
+        name: 'Эгоистичный ген',
+        publicationYear: 2018,
+        price: 530,
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Майкл Сэндел',
+        name: 'Чего не купить за деньги',
+        publicationYear: 2023,
+        price: 390,
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Фрэнк Вильчек',
+        name: 'Красота мира',
+        publicationYear: 2022,
+        price: 480,
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Даниэль Канеман',
+        name: 'Думай медленно… решай быстро',
+        publicationYear: 2017,
+        price: 640,
+        bestseller: true,
+        available: false
+    }
+];
